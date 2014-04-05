6b414c50335b14e9827e2f8a7ba4f825f9a9ee83
Robert Honsby <rhonsby@gmail.com>
2014-04-05 13:26:07 -0700

    Add a method to find chains between a source and a target

--- word_chains.rb
+++ word_chains.rb
@@ -1,20 +1,47 @@
-def adjacent_words(word, dictionary)
-  regexp_pattern = generate_adj_regexp_pattern(word)
+module WordChains
+  extend self
 
-  [].tap do |adj_words|
-    dictionary.each { |word| adj_words << word if word =~ regexp_pattern }
+  def generate_adj_regexp_pattern(word)
+    any_letter = '\w'
+
+    regexp_patterns = []
+    word.split(//).each_index do |idx|
+      pattern = "^#{word[0...idx]}#{any_letter}#{word[idx + 1..-1]}$"
+      regexp_patterns << pattern
+    end
+
+    Regexp.new(regexp_patterns.join('|'))
+  end
+
+  def adjacent_words(word, dictionary)
+    regexp_pattern = generate_adj_regexp_pattern(word)
+
+    [].tap do |adj_words|
+      dictionary.each { |word| adj_words << word if word =~ regexp_pattern }
+    end
+  end
+
+  def find_chain(source, target, dictionary)
+    words_to_expand = [source]
+    words_already_expanded = []
+    candidate_words = dictionary.select { |word| word.length == source.length }
+    parents = { source => nil }
+
+    until words_to_expand.empty?
+      word = words_to_expand.pop
+      adjacent_words = adjacent_words(word, dict)
+
+      adjacent_words.each do |adj_word|
+        unless words_already_expanded.include?(adj_word)
+          words_to_expand << adj_word
+          parents[adj_word] = word
+          return parents if adj_word == target
+        end
+      end
+
+      words_already_expanded << word
+    end
   end
 end
 
-def generate_adj_regexp_pattern(word)
-  any_letter = '\w'
-
-  regexp_patterns = []
-  word.split(//).each_index do |idx|
-    pattern = "^#{word[0...idx]}#{any_letter}#{word[idx + 1..-1]}$"
-    regexp_patterns << pattern
-  end
-
-  Regexp.new(regexp_patterns.join('|'))
-end
-
+WordChains.adjacent_words('cat', ['lat'])
